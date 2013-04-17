839fb14a95607719103bc13dc23b4e5a860df9ee
Mo'men Osama <general_cyclone@hotmail.com>
2013-04-17 17:47:19 +0200

    first commit

---
+++ CustomControls/Controls/MyLabel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ControlsLibrary
+{
+    public class MyLabel : Label
+    {
+        static MyLabel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyLabel), new FrameworkPropertyMetadata(typeof(MyLabel)));
+        }
+    }
+}
